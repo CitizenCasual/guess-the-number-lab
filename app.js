cb9f0948804f5781afc917000f27103a713d4677
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-01-30 12:36:55 -0500

    Make getGuess method

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
-  prevGuesses: [],
+  prevGuesses: [], 
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    while (getGuess()) {
+      prevGuesses.push(this.guess)
+    }
+  }, 
+  getGuess()  {
+    let guess;
+    do {
+      guess = parseInt(prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`))
+    } while (guess >= this.smallestNum && guess <= this.biggestNum);
   },
 }
+
+
 
